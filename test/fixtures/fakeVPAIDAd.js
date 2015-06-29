d124b2bcb399daaf2dd24787217490ae2d80ade0
Luis Silva <luis.dasilva@mailonline.co.uk>
2015-06-29 09:46:01 +0100

    Update 2015-06-29T08:45:59.548Z

--- test/fixtures/fakeVPAIDAd.js
+++
@@ -1,7 +0,0 @@
-//a fake vpaid ad only to pass the validation of IVPAIDAdUnit
-var IVPAIDAdUnit = require('../../js/IVPAIDAdUnit');
-
-window.getVPAIDAd = function() {
-    return new IVPAIDAdUnit();
-}
-
